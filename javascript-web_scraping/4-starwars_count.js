2a40a4fc8c773eda12dc639725b9084bc990ddcf
root <root@LEE.>
2025-04-03 11:11:14 +0300

    second commit

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('Error fetching data:', error);
+        return;
+    }
+
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(film => {
+        if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+            count++;
+        }
+    });
+
+    console.log('Number of movies with Wedge Antilles:', count);
+});
